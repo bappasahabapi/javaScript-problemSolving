a4e74e3cfcdedfa8612233e9a056fd0734afde84
Bappa Saha <bappasaha161@gmail.com>
2023-04-17 01:31:22 +0600

    Update array

--- javaScriptBasic-LSW/21-Array.js
+++ javaScriptBasic-LSW/21-Array.js
@@ -1,1 +1,63 @@
 //todo: 21-array
+
+/* const cars = ["Saab", "Volvo", "BMW",67, {},null,undefined,'last-element-mango'];
+console.log(typeof cars);
+console.log(cars.length);
+const lastElement =cars[cars.length-1];
+console.log({lastElement});
+console.table(cars.sort());
+ */
+
+/* 
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const lastElement =fruits[fruits.length-1];
+
+
+console.table(fruits);
+console.log('Fruits length is:',fruits.length)
+
+console.table(lastElement);
+
+fruits.length=0;
+console.log('Fruits length is:',fruits.length) */
+
+
+// todo: looping array elements
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fLen = fruits.length;
+
+// // let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+// //   text =text+ "<li>" + fruits[i] + "</li>";
+// console.log(i)
+//     console.table(fruits[i]);
+// }
+// // text += "</ul>";
+// console.log('finised');
+
+// todo: using forEach insted of for loop
+
+/* const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+function myFunction(value){
+    console.log(value);
+}
+fruits.forEach(myFunction); */
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fLen = fruits.length;
+
+// fruits.forEach((fruit)=>console.log(fruit));
+
+
+// todo: How to check which one is array
+
+/* const numbers =[1,2,3];
+const person={name:'bappa',age:35};
+
+console.log(typeof numbers, typeof person);
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(person)); */
